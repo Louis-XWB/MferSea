b8468d2ac59d70037700ab2382b016a247fecfb4
Novar <xuwab77@gmail.com>
2024-02-23 00:05:53 +0800

    test launch app

--- src/components/home-comp/HomeBrowser.tsx
+++ src/components/home-comp/HomeBrowser.tsx
@@ -1,9 +1,11 @@
 export default function HomeBrowser(){
     return (
         <div>
-            <a href="mcdonalds://openLogin">打开 mcdonalds APP</a >
+            <a href="mcdonalds://openLogin/login.app">打开 mcdonalds APP</a >
 
-            This is the NFTs browser for article, market, and star.
+            or
+            <br />
+            <a href="mcdonalds://openLogin">打开 mcdonalds APP 2</a >
 
         </div>
     )
