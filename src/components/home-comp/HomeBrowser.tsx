56fe4309e4e1fb18ab1469074b969f8b377d8bc4
Novar <xuwab77@gmail.com>
2024-02-23 01:50:54 +0800

    test launch app

--- src/components/home-comp/HomeBrowser.tsx
+++ src/components/home-comp/HomeBrowser.tsx
@@ -1,11 +1,13 @@
 export default function HomeBrowser(){
     return (
         <div>
-            <a href="http://mcdonalds.com.au">打开 mcdonalds APP</a >
-
-            or
+            <a href="http://mcdonalds.com.au">打开 mcdonalds.com.au APP</a >
             <br />
-            <a href="https://mcdonalds.com.au">打开 mcdonalds APP 2</a >
+            <a href="https://mcdonalds.com.au">打开 mcdonalds.com.au APP 2</a >
+            <br />
+            <a href="http://mcdonaldsca-dev.smart.link">打开 mcdonaldsca-dev.smart.link APP 3</a >
+            <br />
+            <a href="https://mcdonaldsca-dev.smart.link">打开 mcdonaldsca-dev.smart.link APP 4</a >
 
         </div>
     )
